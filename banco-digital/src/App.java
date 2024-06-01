b2cd1fe9823133e7f3a14ad8e2d22253041bb0d1
Beatriz <biabagg12@gmail.com>
2024-06-01 16:42:05 -0300

    basic code

--- banco-digital/src/App.java
+++ banco-digital/src/App.java
@@ -1,5 +1,21 @@
+import model.Cliente;
+import model.Conta;
+import model.ContaCorrente;
+import model.ContaPoupanca;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Cliente beatriz = new Cliente();
+		beatriz.setNome("Beatriz");
+		
+		Conta cc = new ContaCorrente(beatriz);
+		Conta poupanca = new ContaPoupanca(beatriz);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
     }
 }
